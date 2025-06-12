090e4704f6da0d850a04ca08d182987c8ab5d408
Rizkyyy132 <hermawanrizky1212@gmail.com>
2025-06-12 23:22:23 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+
+# 02-jun-25-Rizkyyy132
+
+# rizky-portofolio
+
---
+++ script.js
@@ -0,0 +1,11 @@
+HEAD
+setTimeout(() => {
+  alertBox.classList.add("d-none");
+}, 3000); // 3 detik
+
+
+setTimeout(() => {
+  alertBox.classList.add("d-none");
+}, 3000); // 3 detik
+
+ 
